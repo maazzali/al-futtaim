a4b9da46f9d464e77b1acc6f8ba049f2a413bdc1
Maaz Ali <maaz.ali@keeptruckin.com>
2019-03-20 00:42:39 +0500

    add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
